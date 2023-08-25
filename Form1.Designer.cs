9c1b45e63beddebf6dda60dbd7bcb731766d8bd7
root <mgiraudo@ies21.edu.ar>
2023-08-25 13:08:21 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿
+namespace Gestion_de_Gastos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtpFecha = new System.Windows.Forms.DateTimePicker();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.cboConcepto = new System.Windows.Forms.ComboBox();
+            this.lblConcepto = new System.Windows.Forms.Label();
+            this.lblImporte = new System.Windows.Forms.Label();
+            this.txtmporte = new System.Windows.Forms.TextBox();
+            this.btnGrabar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dtpFecha
+            // 
+            this.dtpFecha.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpFecha.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpFecha.Location = new System.Drawing.Point(126, 30);
+            this.dtpFecha.Name = "dtpFecha";
+            this.dtpFecha.Size = new System.Drawing.Size(131, 23);
+            this.dtpFecha.TabIndex = 0;
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFecha.Location = new System.Drawing.Point(20, 30);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(47, 17);
+            this.lblFecha.TabIndex = 1;
+            this.lblFecha.Text = "Fecha";
+            // 
+            // cboConcepto
+            // 
+            this.cboConcepto.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboConcepto.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboConcepto.FormattingEnabled = true;
+            this.cboConcepto.Location = new System.Drawing.Point(126, 92);
+            this.cboConcepto.Name = "cboConcepto";
+            this.cboConcepto.Size = new System.Drawing.Size(131, 24);
+            this.cboConcepto.TabIndex = 2;
+            // 
+            // lblConcepto
+            // 
+            this.lblConcepto.AutoSize = true;
+            this.lblConcepto.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblConcepto.Location = new System.Drawing.Point(20, 92);
+            this.lblConcepto.Name = "lblConcepto";
+            this.lblConcepto.Size = new System.Drawing.Size(68, 17);
+            this.lblConcepto.TabIndex = 3;
+            this.lblConcepto.Text = "Concepto";
+            // 
+            // lblImporte
+            // 
+            this.lblImporte.AutoSize = true;
+            this.lblImporte.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblImporte.Location = new System.Drawing.Point(20, 147);
+            this.lblImporte.Name = "lblImporte";
+            this.lblImporte.Size = new System.Drawing.Size(55, 17);
+            this.lblImporte.TabIndex = 4;
+            this.lblImporte.Text = "Importe";
+            // 
+            // txtmporte
+            // 
+            this.txtmporte.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtmporte.Location = new System.Drawing.Point(126, 147);
+            this.txtmporte.MaxLength = 10;
+            this.txtmporte.Name = "txtmporte";
+            this.txtmporte.Size = new System.Drawing.Size(131, 23);
+            this.txtmporte.TabIndex = 5;
+            // 
+            // btnGrabar
+            // 
+            this.btnGrabar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGrabar.Location = new System.Drawing.Point(160, 204);
+            this.btnGrabar.Name = "btnGrabar";
+            this.btnGrabar.Size = new System.Drawing.Size(97, 29);
+            this.btnGrabar.TabIndex = 6;
+            this.btnGrabar.Text = "Grabar";
+            this.btnGrabar.UseVisualStyleBackColor = true;
+            this.btnGrabar.Click += new System.EventHandler(this.btnGrabar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 245);
+            this.Controls.Add(this.btnGrabar);
+            this.Controls.Add(this.txtmporte);
+            this.Controls.Add(this.lblImporte);
+            this.Controls.Add(this.lblConcepto);
+            this.Controls.Add(this.cboConcepto);
+            this.Controls.Add(this.lblFecha);
+            this.Controls.Add(this.dtpFecha);
+            this.Name = "Form1";
+            this.Text = "Gestion de Gastos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dtpFecha;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.ComboBox cboConcepto;
+        private System.Windows.Forms.Label lblConcepto;
+        private System.Windows.Forms.Label lblImporte;
+        private System.Windows.Forms.TextBox txtmporte;
+        private System.Windows.Forms.Button btnGrabar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO; 
+
+namespace Gestion_de_Gastos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+            StreamReader sr = new StreamReader("Conceptos.txt");
+            string linea = "";
+            while (sr.EndOfStream== false)
+            {
+
+                linea = sr.ReadLine();
+                cboConcepto.Items.Add(linea);
+
+            }
+
+            sr.Close();
+            sr.Dispose();
+            
+        }
+
+        private void btnGrabar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Decimal importe = Convert.ToDecimal(txtmporte.Text);
+                if (importe > 0)
+                {
+                    StreamWriter sw = new StreamWriter("Gastos.txt", true);
+
+                    sw.Write(dtpFecha.Text);
+                    sw.Write(",", " ");
+                    sw.Write(cboConcepto.Text);
+                    sw.Write(",", " ");
+                    sw.WriteLine(txtmporte.Text);
+
+                    sw.Close();
+                    sw.Dispose();
+                    cboConcepto.SelectedIndex = 0;
+                    txtmporte.Text = " ";
+
+                }
+                else 
+                {
+                    MessageBox.Show("Ingrese un valor mayor a 0", "AVISO");
+                }
+
+
+                
+
+            } 
+            catch (Exception ex)
+            {
+                MessageBox.Show("Verifique los datos ingresados","ERROR");
+            }
+            
+        }
+    }
+}
